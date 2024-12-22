f0c3828a282e095a3668982c5d37d03c587463e8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-22 23:51:55 +0530

    files

--- weather-app-project-react/src/SearchBox.jsx
+++ weather-app-project-react/src/SearchBox.jsx
@@ -5,14 +5,22 @@ import { useState } from 'react';
 
 export default function SearchBox(){
     let [city, setCity] = useState("");
+
     let handleChange = (event) =>{
         setCity(event.target.value);
     };
+
+    let handleSubmit = (event) =>{
+        event.preventDefault(); //to prevent default behavior of event object
+        console.log(city);
+        setCity("");
+    };
+
     return(
         <div className="SearchBox">
             <h3>Search for the weather</h3>
-            <form>
-            <TextField id="city" label="City Name" variant="outlined" required value={city} />
+            <form onSubmit={handleSubmit}>
+            <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange} />
             <br></br><br></br>
             <Button variant="contained" type="submit">Search</Button>
             </form>
