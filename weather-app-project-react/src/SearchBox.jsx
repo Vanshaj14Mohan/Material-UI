341b2872a6473490ab6cc6e13fa68878f9b49c0b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-22 23:31:54 +0530

    files

--- weather-app-project-react/src/SearchBox.jsx
+++ weather-app-project-react/src/SearchBox.jsx
@@ -1,7 +1,21 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+import { useState } from 'react';
+
 export default function SearchBox(){
+    let [city, setCity] = useState("");
+    let handleChange = (event) =>{
+        setCity(event.target.value);
+    };
     return(
-        <div>
-            <h3>Search for box</h3>
+        <div className="SearchBox">
+            <h3>Search for the weather</h3>
+            <form>
+            <TextField id="city" label="City Name" variant="outlined" required value={city} />
+            <br></br><br></br>
+            <Button variant="contained" type="submit">Search</Button>
+            </form>
         </div>
     );
 }
