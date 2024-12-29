dc9062211e770474a04c8622db35ce3b940a525b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-29 20:00:33 +0530

    files

--- weather-app-project-react/src/InfoBox.jsx
+++ weather-app-project-react/src/InfoBox.jsx
@@ -1,5 +1,14 @@
 //Gonna be our information box
 export default function InfoBox(){
+   let info = {
+    feelsLike: 17.38,
+    humidity: 61,
+    temp: 17.94,
+    tempMax: 17.94,
+    tempMin: 17.94,
+    weather: "clear sky",
+   };
+
     return(
         <div className="Info">
         <h2>Weather Information</h2>
