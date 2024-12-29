497df1f74827b318d39f65f3a2f656310bcd8590
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-29 21:22:43 +0530

    files

--- weather-app-project-react/src/InfoBox.jsx
+++ weather-app-project-react/src/InfoBox.jsx
@@ -1,9 +1,7 @@
 //Gonna be our information box
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function InfoBox(){
@@ -31,9 +29,12 @@ export default function InfoBox(){
         <Typography gutterBottom variant="h5" component="div">
           {info.city}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+          <p>Temperature = {info.temp}&deg;C</p>
+          <p>Humidity = {info.humidity}</p>
+          <p>Min Temp = {info.tempMin}&deg;C</p>
+          <p>Max Temp = {info.tempMax}&deg;C</p>
+          <p>The weather can be described as <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C</p>
         </Typography>
       </CardContent>
     </Card>
