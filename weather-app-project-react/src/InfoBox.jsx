493e85b20f062f6815f064945ef350f1a5433832
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-29 20:49:40 +0530

    files

--- weather-app-project-react/src/InfoBox.jsx
+++ weather-app-project-react/src/InfoBox.jsx
@@ -1,6 +1,15 @@
 //Gonna be our information box
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 export default function InfoBox(){
+    const INIT_URL = "https://images.unsplash.com/photo-1545134969-8debd725b007?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
    let info = {
+    city: "Lucknow",
     feelsLike: 17.38,
     humidity: 61,
     temp: 17.94,
@@ -11,7 +20,23 @@ export default function InfoBox(){
 
     return(
         <div className="Info">
-        <h2>Weather Information</h2>
+        <h2>Weather Information - {info.weather} </h2>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={INIT_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
         </div>
     );
 }
