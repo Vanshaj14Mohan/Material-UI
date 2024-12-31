f3fe293ea1a6fa156728ff5fcf0c4fd1ee25c72d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-31 17:41:58 +0530

    files

--- weather-app-project-react/src/InfoBox.jsx
+++ weather-app-project-react/src/InfoBox.jsx
@@ -10,11 +10,11 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 export default function InfoBox({info}){
     const INIT_URL = "https://images.unsplash.com/photo-1545134969-8debd725b007?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
-  
+    //For different weather conditions..
     const HOT_URL = "https://images.unsplash.com/photo-1581129724980-2ab2153c3d8d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D"; 
     const COLD_URL = "https://images.unsplash.com/photo-1674407866481-a39b2239f771?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
     const RAIN_URL = "https://plus.unsplash.com/premium_photo-1666717576644-5701d3406840?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFpbnklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
-
+   //Three types of weather Hot, Cold & Rainy
     return(
         <div className="Info">
         <div className="cardContainer">
